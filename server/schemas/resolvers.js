1be6c66e3be8234b8a7028c85b361fb07f7b565e
Spiewak <bspiewak6@gmail.com>
2021-01-12 17:02:47 -0500

    need to fix the token bug

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -5,6 +5,19 @@ const { sign } = require('jsonwebtoken');
 
 const resolvers = {
     Query: {
+        me: async (parent, args, context) => {
+            if (context.user) {
+                const userData = await User.findOne({ _id: context.user._id })
+                    .select('-__v -password')
+                    .populate('thoughts')
+                    .populate('friends');
+                    
+                return userData;
+            }
+            
+            throw new AuthenticationError('Not logged in');
+        },
+
         thoughts: async (parent, { username }) => {
             const params = username ? { username } : {};
             return Thought.find(params).sort({ createdAt: -1 });
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -4,6 +4,15 @@ const { gql } = require('apollo-server-express');
 // create our typeDefs
 const typeDefs = gql`
 
+    type User {
+        _id: ID
+        username: String
+        email: String
+        friendCount: Int
+        thoughts: [Thought]
+        friends: [User]
+    }
+
     type Thought {
         _id: ID
         thoughtText: String
@@ -20,16 +29,8 @@ const typeDefs = gql`
         username: String
     }
 
-    type User {
-        _id: ID
-        username: String
-        email: String
-        friendCount: Int
-        thoughts: [Thought]
-        friends: [User]
-    }
-
     type Query {
+        me: User
         users: [User]
         user(username: String!): User
         thoughts(username: String): [Thought]
