1479123b95aeac5df71ace2b5393f7db4bd71be5
Spiewak <bspiewak6@gmail.com>
2021-01-14 17:46:48 -0500

    setup profile with username, thoughts and friends

--- client/src/components/ReactionList/index.js
+++ client/src/components/ReactionList/index.js
@@ -3,10 +3,23 @@ import { Link } from 'react-router-dom';
 
 const ReactionList = ({ reactions }) => {
     return (
-        <div>
-            
+        <div className="card mb-3">
+            <div className="card-header">
+                <span className="text-light">Reactions</span>
+            </div>
+            <div className="card-body">
+                {reactions && 
+                reactions.map(reaction => (
+                    <p className="pill mb-3" key={reaction.id}>
+                        {reaction.reactionBody} {'// '}
+                        <Link to={`/profile/${reaction.username}`} style={{ fontWeight: 700 }}>
+                            {reaction.username} on {reaction.createdAt}
+                        </Link>
+                    </p>                    
+                ))}
+            </div>
         </div>
-    );
+    )
 };
 
 export default ReactionList;
